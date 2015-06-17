c585be666dac189bdd4a536ec4058e853d147620
Lars Meyer <lars.meyer@uni-weimar.de>
2015-06-17 16:35:55 +0200

    Externalize Sphere methods.

---
+++ framework/sphere.cpp
@@ -0,0 +1,43 @@
+#include "sphere.hpp"
+
+Sphere::Sphere(): // default constructor
+        center_{0.0, 0.0, 0.0},
+        radius_{0.0}
+        {}
+
+Sphere::Sphere(Sphere const& s): // copy constructor
+        center_{s.center_},
+        radius_{s.radius_}
+        {}
+
+Sphere::Sphere(Sphere&& s): // move constructor, copy & swap
+        Sphere()
+        {
+                swap(*this, s);
+        }
+
+Sphere::Sphere(glm::vec3 const& c, double r):
+        center_{c},
+        radius_{r}
+        {}
+
+friend void Sphere::swap(Sphere & s1, Sphere & s2) {
+        std::swap(s1.center_, s2.center_);
+        std::swap(s1.radius_, s2.radius_);
+}
+
+double Sphere::getRadius() const {
+        return radius_;
+}
+
+glm::vec3 Sphere::getCenter() const {
+        return center_;
+}
+
+/* virtual */ double Sphere::area() const {
+        return (4 * M_PI * radius_ * radius_);
+}
+
+/* virtual */ double Sphere::volume() const {
+        return ((4.0/3.0) * M_PI * radius_ * radius_ * radius_);
+}
--- framework/sphere.hpp
+++ framework/sphere.hpp
@@ -6,44 +6,20 @@
 
 class Sphere: public Shape {
 public:
-        Sphere(): // default constructor
-                center_{0.0, 0.0, 0.0},
-                radius_{0.0}
-                {}
-        Sphere(Sphere const& s): // copy constructor
-                center_{s.center_},
-                radius_{s.radius_}
-                {}
-        Sphere(Sphere&& s): // move constructor, copy & swap
-                Sphere()
-                {
-                        swap(*this, s);
-                }
-        Sphere(glm::vec3 const& c, double r):
-                center_{c},
-                radius_{r}
-                {}
+        Sphere();
+        Sphere(Sphere const& s);
+        Sphere(Sphere&& s);
+        Sphere(glm::vec3 const& c, double r);
 
-        friend void swap(Sphere & s1, Sphere & s2) {
-                std::swap(s1.center_, s2.center_);
-                std::swap(s1.radius_, s2.radius_);
-        }
+        friend void swap(Sphere & s1, Sphere & s2);
+        
+        double getRadius() const;
 
-        double getRadius() const {
-                return radius_;
-        }
+        glm::vec3 getCenter() const;
 
-        glm::vec3 getCenter() const {
-                return center_;
-        }
+        /* virtual */ double area() const;
 
-        /* virtual */ double area() const {
-                return (4 * M_PI * radius_ * radius_);
-        }
-
-        /* virtual */ double volume() const {
-                return ((4.0/3.0) * M_PI * radius_ * radius_ * radius_);
-        }
+        /* virtual */ double volume() const;
 private:
         glm::vec3 center_;
         double radius_;
