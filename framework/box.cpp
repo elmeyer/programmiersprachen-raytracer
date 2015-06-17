57c9c9b05c508a788c84c3dfc967e62a3f41e338
Lars Meyer <lars.meyer@uni-weimar.de>
2015-06-17 16:56:31 +0200

    Define class Box.

---
+++ framework/box.cpp
@@ -0,0 +1,32 @@
+#include "box.hpp"
+
+Box::Box(): // default constructor
+        min_{0.0, 0.0, 0.0},
+        max_{0.0, 0.0, 0.0}
+        {}
+Box::Box(Box const& b): // copy constructor
+        min_{b.min_},
+        max_{b.max_}
+        {}
+Box::Box(Box&& b): // move constructor
+        Box()
+        {
+                swap(*this, b);
+        }
+Box::Box(glm::vec3 const& min, glm::vec3 const& max):
+        min_{min},
+        max_{max}
+        {}
+
+friend void Box::swap(Box & b1, Box & b2) {
+        std::swap(b1.min_, b2.min_);
+        std::swap(b1.max_, b2.max_);
+}
+
+glm::vec3 Box::getMin() const {
+        return min_;
+}
+
+glm::vec3 Box::getMax() const {
+        return max_;
+}
