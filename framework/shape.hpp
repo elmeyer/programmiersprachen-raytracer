cc7dc77d5b71b68c40292cc1453c333028657734
Lars Meyer <lars.meyer@uni-weimar.de>
2015-06-17 16:23:04 +0200

    Add constructor declarations.

--- framework/shape.hpp
+++ framework/shape.hpp
@@ -3,6 +3,9 @@
 
 class Shape {
 public:
+        Shape();
+        Shape(Shape const& s);
+        Shape(Shape&& s);
         virtual double area() const = 0;
         virtual double volume() const = 0;
 };
