4a52e02ad31faf8b42e47fe183bd7e8d02b90e77
Lars Meyer <lars.meyer@uni-weimar.de>
2015-06-17 16:07:28 +0200

    Add class Sphere and constructors.

---
+++ framework/sphere.hpp
@@ -0,0 +1,35 @@
+#ifndef BUW_SPHERE_HPP
+#define BUW_SPHERE_HPP
+
+#include <glm/vec3.hpp>
+
+class Sphere: public Shape {
+public:
+        Sphere(): // default constructor
+                center_{0.0, 0.0, 0.0},
+                radius_{0.0}
+                {}
+        Sphere(Sphere const& s): // copy constructor
+                center_{s.center_},
+                radius_{s.radius_}
+                {}
+        Sphere(Sphere&& s): // move constructor, copy & swap
+                Sphere()
+                {
+                        swap(*this, s);
+                }
+        Sphere(glm::vec3 const& c, double r):
+                center_{c},
+                radius_{r}
+                {}
+
+        friend void swap(Sphere & s1, Sphere & s2) {
+                std::swap(s1.center_, s2.center_);
+                std::swap(s1.radius_, s2.radius_);
+        }
+private:
+        glm::vec3 center_;
+        double radius_;
+};
+
+#endif
