e4ccbdf5de4efa51a237571405a3cd3300cbc0a0
Lars Meyer <lars.meyer@uni-weimar.de>
2015-06-17 18:24:09 +0200

    Add name and color.

--- framework/shape.hpp
+++ framework/shape.hpp
@@ -1,10 +1,16 @@
 #ifndef BUW_SHAPE_HPP
 #define BUW_SHAPE_HPP
 
+#include <string>
+#include "color.hpp"
+
 class Shape {
 public:
         virtual double area() const = 0;
         virtual double volume() const = 0;
+private:
+        std::string name_;
+        Color color_;
 };
 
 #endif
