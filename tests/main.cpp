d78f9bf03dd569979d06977f0d460825694d6b8d
Lars Meyer <lars.meyer@uni-weimar.de>
2015-06-17 17:32:06 +0200

    Added tests for sphere.

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,35 +5,40 @@
 
 TEST_CASE("Sphere default construction", "[constructor]") {
         Sphere s{};
+        glm::vec3 c{0.0, 0.0, 0.0};
         REQUIRE(s.getRadius() == 0.0);
-        REQUIRE(s.getCenter() == {0.0, 0.0, 0.0});
+        REQUIRE(s.getCenter() == c);
 }
 
 TEST_CASE("Sphere construction", "[constructor]") {
-        Sphere s{{1.0, 1.0, 0.0}, 5.0};
+        glm::vec3 c{1.0, 1.0, 1.0};
+        Sphere s{c, 5.0};
         REQUIRE(s.getRadius() == 5.0);
-        REQUIRE(s.getCenter() == {1.0, 1.0, 1.0});
+        REQUIRE(s.getCenter() == c);
 }
 
 TEST_CASE("Sphere radius-only construction", "[constructor]") {
         Sphere s{5.0};
+        glm::vec3 c{0.0, 0.0, 0.0};
         REQUIRE(s.getRadius() == 5.0);
-        REQUIRE(s.getCenter() == {0.0, 0.0, 0.0});
+        REQUIRE(s.getCenter() == c);
 }
 
 TEST_CASE("Sphere copy construction", "[constructor]") {
         Sphere s1{5.0};
         Sphere s2(s1);
-        REQUIRE(s2 == s1);
+        REQUIRE(s2.getRadius() == s1.getRadius());
+        REQUIRE(s2.getCenter() == s1.getCenter());
 }
 
 TEST_CASE("Sphere move construction", "[constructor]") {
         Sphere s1{5.0};
         Sphere s2(std::move(s1));
+        glm::vec3 c{0.0, 0.0, 0.0};
         REQUIRE(s2.getRadius() == 5.0);
-        REQUIRE(s2.getCenter() == {0.0, 0.0, 0.0});
+        REQUIRE(s2.getCenter() == c);
         REQUIRE(s1.getRadius() == 0.0);
-        REQUIRE(s1.getCenter() == {0.0, 0.0, 0.0});
+        REQUIRE(s1.getCenter() == c);
 }
 
 TEST_CASE("Sphere area calculation", "[area]") {
