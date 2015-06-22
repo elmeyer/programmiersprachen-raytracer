7a56ca627b52b61339efa2f020583ca0b263fb09
Lars Meyer <lars.meyer@uni-weimar.de>
2015-06-22 16:50:50 +0200

    Add dummy for exc. 4.

--- framework/shape.hpp
+++ framework/shape.hpp
@@ -10,13 +10,21 @@ public:
         Shape(std::string const& name);
         Shape(Color const& color);
         Shape(std::string const& name, Color const& color);
+
         std::string getName() const;
         Color getColor() const;
+
         virtual double area() const = 0;
         virtual double volume() const = 0;
+
+        virtual std::ostream& print(std::ostream& os) const;
 private:
         std::string name_;
         Color color_;
 };
+
+std::ostream& operator<<(std::ostream& os, Shape const& s){
+        // not implemented yet
+}
 
 #endif
