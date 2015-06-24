49a06815263e5664fc595e6ce31c8e014fea92ee
Lars Meyer <lars.meyer@uni-weimar.de>
2015-06-24 16:36:18 +0200

    Add test case for exc 6.6.

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,6 +2,8 @@
 #include <catch.hpp>
 #include "sphere.hpp"
 #include "box.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtx/intersect.hpp>
 
 TEST_CASE("Sphere default construction", "[constructor]") {
         Sphere s{};
@@ -103,6 +105,26 @@ TEST_CASE("Printing a Sphere", "[<<]") {
 TEST_CASE("Printing a Box", "[<<]") {
         Box b("Test", Color{0.0, 1.0, 0.0});
         std::cout << b << std::endl;
+}
+
+TEST_CASE("intersectRaySphere", "[intersect]") {
+        // Ray
+        glm::vec3 ray_origin(0.0,0.0,0.0);
+        // ray direction has to be normalized!
+        // you can use:
+        //      v = glm::normalize(some_vector)
+        glm::vec3 ray_direction(0.0,0.0,1.0);
+
+        // Sphere
+        glm::vec3 sphere_center(0.0,0.0,5.0);
+        float sphere_radius(1.0);
+
+        float distance(0.0);
+        auto result = glm::intersectRaySphere(
+                ray_origin, ray_direction,
+                sphere_center, sphere_radius,
+                distance);
+        REQUIRE(distance == Approx(4.0f));
 }
 
 int main(int argc, char *argv[])
