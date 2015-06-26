ae90044b03412564526551f3b57612c5379ed399
Lars Meyer <lars.meyer@uni-weimar.de>
2015-06-26 11:46:41 +0200

    Add deconstructor and constructor messages.

--- framework/shape.cpp
+++ framework/shape.cpp
@@ -3,22 +3,26 @@
 Shape::Shape():
         name_{},
         color_{0.0, 0.0, 0.0}
-        {}
+        {std::cout << "Construct a Shape!" << "\n";}
+
+/* virtual */ Shape::~Shape() {
+        std::cout << "Destroy the Shape!" << "\n";
+}
 
 Shape::Shape(std::string const& name):
         name_{name},
         color_{1.0, 1.0, 1.0}
-        {}
+        {std::cout << "Construct a Shape!" << "\n";}
 
 Shape::Shape(Color const& color):
         name_{},
         color_{color}
-        {}
+        {std::cout << "Construct a Shape!" << "\n";}
 
 Shape::Shape(std::string const& name, Color const& color):
         name_{name},
         color_{color}
-        {}
+        {std::cout << "Construct a Shape!" << "\n";}
 
 std::string Shape::getName() const {
         return name_;
--- framework/shape.hpp
+++ framework/shape.hpp
@@ -8,6 +8,7 @@
 class Shape {
 public:
         Shape();
+        /* virtual */ ~Shape();
         Shape(std::string const& name);
         Shape(Color const& color);
         Shape(std::string const& name, Color const& color);
